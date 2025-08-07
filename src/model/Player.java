ecece8f42f998c11203748d606e3714808fbaa45
Padma Sai <padmasaigorrela@mial.com>
2025-08-07 11:16:45 +0530

    model commit

---
+++ src/model/Player.java
@@ -0,0 +1,27 @@
+package model;
+
+public class Player {
+    private String name;
+    private String role; // e.g., Batsman, Bowler, All-Rounder
+
+    public Player(String name, String role) {
+        this.name = name;
+        this.role = role;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
